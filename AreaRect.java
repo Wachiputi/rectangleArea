3bc1da27f8c8299a5c7cf5a43b4f8ab0ca877ce3
wachiputi <bsc-ele-29-19@unima.ac.mw>
2021-12-14 11:51:52 +0200

    added a second file

---
+++ AreaRect.java
@@ -0,0 +1,12 @@
+class AreaRect{
+    double length =1;
+    double breadth = 1;
+
+    AreaRect(){
+
+    }
+    public double getReturnArea(){
+        double ar = length * breadth;
+        return ar;
+    }
+}
--- area.java
+++ area.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class area{
     public static void main(String args []){
-        System.out.println("hello world");
+        System.out.println("The area will show here ");
+        //creates a Scanner object
+        //creates an object of class AreaRect
+        Scanner input = new Scanner(System.in);
+        AreaRect c1 = new AreaRect();
     }
 }
